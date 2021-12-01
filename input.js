22bb206d07b7c8feeb59c9b3ab9f2817b23b787f
Selene Grossett <selene_816@hotmail.com>
2021-12-01 23:48:46 +0000

    used the ES6 object shorthand notation to add the setupInput function to input.js's module.exports object

---
+++ input.js
@@ -0,0 +1,20 @@
+const { builtinModules } = require("module");
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+
+const handleUserInput = function (data) {
+  // \u0003 maps to ctrl+c input
+  if (data === '\u0003') process.exit();
+};
+
+module.exports = { setupInput };

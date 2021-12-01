9a2d96770cd9ad8bc9f2c5bc508040704b0d66f5
Selene Grossett <selene_816@hotmail.com>
2021-12-01 23:48:15 +0000

    imported setupInput function from input.js

--- play.js
+++ play.js
@@ -1,24 +1,10 @@
 const { stdin } = require('process');
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 
 console.log("Connecting ...");
 connect();
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-
-const handleUserInput = function (data) {
-  // \u0003 maps to ctrl+c input
-  if (data === '\u0003') process.exit();
-};
 
 setupInput();

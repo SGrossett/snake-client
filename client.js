1af6eea330f382bf9f59a1ca88cadf01bd93bbf6
Selene Grossett <selene_816@hotmail.com>
2021-12-02 02:37:39 +0000

    modified the connect function to use the new constrants and cleaned up the comments

--- client.js
+++ client.js
@@ -1,27 +1,21 @@
 const net = require('net');
+const { IP, PORT } = require("./constants");
 
-// establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: '165.227.47.243', 
-    port: 50541
+    host: IP, 
+    port : PORT
   });
 
-  // interpret incoming data as text
   conn.setEncoding("utf8");
   
   conn.on('data', (data) => {
-    console.log(data);
+    console.log("Server:", data);
   });
 
   conn.on('connect', () => {
     console.log("Successfully connected to game server");
     conn.write("Name: SSG");
-    /*
-    setTimeout( () => {
-      conn.write("Move: up");
-    }, 500)
-    */
   });
   
   return conn;
